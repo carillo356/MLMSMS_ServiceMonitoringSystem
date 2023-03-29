50a8782ad95e1ad3e711e474d2d7fa720edd12e5
Mintchi01 <127159876+Mintchi01@users.noreply.github.com>
2023-03-29 11:43:31 +0800

    Major Changes

--- LoginAndRegisterASPMVC5/JS_Scripts/admin_Users/pagination.js
+++ LoginAndRegisterASPMVC5/JS_Scripts/admin_Users/pagination.js
@@ -1,95 +1,73 @@
 ﻿//Pagination for Users Table
 
 // define the number of items per page
- 
-let ITEMS_PER_PAGE = 5;
+//let ITEMS_PER_PAGE = 999;
 
+//    // Set up the event listener for options
+//    $('.options_AdminRow li').click(function () {
+//        var optionText = $(this).find('.option_Admin-text').text();
+//        switch (optionText) {
+//            case "All rows":
+//                ITEMS_PER_PAGE = -1;
+//                break;
+//            default:
+//                ITEMS_PER_PAGE = parseInt(optionText);
+//                break;
+//        }
 
-    // Set up the event listener for options
-    $('.options_AdminRow li').click(function () {
-        var optionText = $(this).find('.option_Admin-text').text();
-        switch (optionText) {
-            case "All rows":
-                ITEMS_PER_PAGE = -1;
-                break;
-            default:
-                ITEMS_PER_PAGE = parseInt(optionText);
-                break;
-        }
-        showPage(1); // show the first page of the updated table
-    });
-
-
-// get the table body element
-const tableBody = document.querySelector('#usersTable tbody');
-
-// get the pagination links
-const paginationLinks = document.querySelectorAll('.pagination li');
-
-// define a function to show the items for the selected page
-function showPage(pageNumber) {
-    // calculate the start and end indexes of the items to show
-    const startIndex = (pageNumber - 1) * ITEMS_PER_PAGE;
-    const endIndex = startIndex + ITEMS_PER_PAGE;
-
-    // hide all rows in the table
-    const rows = tableBody.querySelectorAll('tr');
-    rows.forEach(row => {
-        row.style.display = 'none';
-    });
-
-    // show the rows for the current page
-    for (let i = startIndex; i < endIndex && i < rows.length; i++) {
-        rows[i].style.display = '';
-    }
-}
-
-
-//Event Listener for Selecting no. of Rows
-
-
-
-// /*handle the click event of the pagination links*/
-//paginationLinks.forEach(link => {
-//    link.addEventListener('click', event => {
-//        event.preventDefault();
-//        // get the selected page number from the link's text
-//        pageNumber = parseInt(link.innerText);
-
-//        // show the items for the selected page
-//        showPage(pageNumber);
-
-//        // mark the selected page as active
-//        paginationLinks.forEach(link => {
-//            link.parentElement.classList.remove('active');
-//        });
-//        link.parentElement.classList.add('active');
+//        showPage(1); // show the first page of the updated table
 //    });
 
 
-//// show the first page by default
+//// get the table body element
+//const tableBody = document.querySelector('#usersTable tbody');
+
+//// get the pagination links
+//const paginationLinks = document.querySelectorAll('.pagination li');
+
+//// define a function to show the items for the selected page
+//function showPage(pageNumber) {
+//    // calculate the start and end indexes of the items to show
+//    const startIndex = (pageNumber - 1) * ITEMS_PER_PAGE;
+//    const endIndex = startIndex + ITEMS_PER_PAGE;
+
+//    // hide all rows in the table
+//    const rows = tableBody.querySelectorAll('tr');
+//    rows.forEach(row => {
+//        row.style.display = 'none';
+//    });
+
+//    // show the rows for the current page
+//    for (let i = startIndex; i < endIndex && i < rows.length; i++) {
+//        rows[i].style.display = '';
+//    }
+//}
+
+
 //$(document).ready(function () {
-//    //RealTimeUsersTable().then(function () {
-//    //    showPage(1);
-//    /*    });*/
 //    Synch();
 //});
 
 
+//function Synch() {
+//    RealTimeUsersTable();
+//        //.then(function () {
+//        //    showPage(1); // call showPage after reloading the table
+//        //})
+//        //.catch(function (error) {
+//        //    alert(error);
+//        //});
+//}
+
 $(document).ready(function () {
-    Synch();
+    RealTimeUsersTable().then(function (result) {
+        const data = { "req_per_page": document.getElementById("req_per_page").value, "page_no": 1 };
+        pagination(data, result);
+    }).catch(function (error) {
+        alert(error);
+    });
 });
 
-
-function Synch() {
-    RealTimeUsersTable()
-        .then(function () {
-            showPage(1); // call showPage after reloading the table
-        })
-        .catch(function (error) {
-            alert(error);
-        });
-}
 
 
 function RealTimeUsersTable() {
@@ -185,156 +163,3 @@ function setNotification(IdUser, FirstName, LastName, command) {
     });
 }
       
-
-//let currentPage = 1;
-//const itemsPerPage = 5;
-//const totalPages = Math.ceil(totalItems / itemsPerPage);
-
-//function renderPagination() {
-//    let paginationHTML = '';
-//    if (currentPage === 1) {
-//        paginationHTML += '<li class="page-item disabled"><a class="page-link" href="#">Prev</a></li>';
-//    } else {
-//        paginationHTML += '<li class="page-item"><a class="page-link" href="#" onclick="prevPage()">Prev</a></li>';
-//    }
-
-//    for (let i = 1; i <= totalPages; i++) {
-//        if (i === currentPage) {
-//            paginationHTML += '<li class="page-item active"><a class="page-link" href="#" onclick="goToPage(' + i + ')">' + i + '</a></li>';
-//        } else {
-//            paginationHTML += '<li class="page-item"><a class="page-link" href="#" onclick="goToPage(' + i + ')">' + i + '</a></li>';
-//        }
-//    }
-
-//    if (currentPage === totalPages) {
-//        paginationHTML += '<li class="page-item disabled"><a class="page-link" href="#">Next</a></li>';
-//    } else {
-//        paginationHTML += '<li class="page-item"><a class="page-link" href="#" onclick="nextPage()">Next</a></li>';
-//    }
-
-//    document.getElementById('pagination').innerHTML = paginationHTML;
-//}
-
-//function goToPage(page) {
-//    currentPage = page;
-//    renderPagination();
-//    showPage(page);
-//}
-
-//function nextPage() {
-//    if (currentPage < totalPages) {
-//        currentPage++;
-//        renderPagination();
-//        showPage(currentPage);
-//    }
-//}
-
-//function prevPage() {
-//    if (currentPage > 1) {
-//        currentPage--;
-//        renderPagination();
-//        showPage(currentPage);
-//    }
-//}
-
-//renderPagination();
-//showPage(currentPage);
-
-//const pagination = document.querySelector('.pagination');
-//const links = pagination.querySelectorAll('.page-link');
-
-//function updatePagination(start) {
-//    for (let i = 0; i < links.length; i++) {
-//        if (i === 0 || i === links.length - 1 || (i >= start && i < start + 5)) {
-//            links[i].style.display = 'inline-block';
-//        } else {
-//            links[i].style.display = 'none';
-//        }
-//    }
-//}
-
-//updatePagination(1);
-
-//pagination.addEventListener('click', event => {
-//    event.preventDefault();
-//    if (event.target.classList.contains('page-link')) {
-//        const current = pagination.querySelector('.active');
-//        if (event.target.innerText === 'Next') {
-//            if (current.nextElementSibling) {
-//                current.nextElementSibling.classList.add('active');
-//                current.classList.remove('active');
-//                updatePagination(parseInt(current.nextElementSibling.innerText) - 1);
-//            }
-//        } else if (event.target.innerText === 'Previous') {
-//            if (current.previousElementSibling) {
-//                current.previousElementSibling.classList.add('active');
-//                current.classList.remove('active');
-//                updatePagination(parseInt(current.previousElementSibling.innerText) - 1);
-//            }
-//        } else {
-//            current.classList.remove('active');
-//            event.target.parentElement.classList.add('active');
-//            updatePagination(parseInt(event.target.innerText) - 1);
-//        }
-//    }
-//});
-
-
-//const paginationLinks = document.querySelectorAll('.pagination .page-item a');
-//const previousLink = document.querySelector('#previous-link');
-//const nextLink = document.querySelector('#next-link');
-//let currentPage = 1;
-
-//// initialize pagination links
-//paginationLinks.forEach((link, index) => {
-//    if (index > 0 && index < 6) {
-//        link.textContent = index + 1;
-//    } else {
-//        link.parentElement.style.display = 'none';
-//    }
-//});
-
-//previousLink.addEventListener('click', () => {
-//    if (currentPage > 1) {
-//        currentPage--;
-//        updatePaginationLinks();
-//    }
-//});
-
-//nextLink.addEventListener('click', () => {
-//    currentPage++;
-//    updatePaginationLinks();
-//});
-
-//function updatePaginationLinks() {
-//    // update page numbers
-//    paginationLinks.forEach((link, index) => {
-//        if (index > 0 && index < 6) {
-//            link.textContent = currentPage + (index - 1);
-//        }
-//    });
-
-//    // show/hide links based on current page
-//    if (currentPage === 1) {
-//        previousLink.style.display = 'none';
-//        nextLink.style.display = '';
-//    } else if (currentPage === 6) {
-//        previousLink.style.display = '';
-//        nextLink.style.display = 'none';
-//    } else {
-//        previousLink.style.display = '';
-//        nextLink.style.display = '';
-//    }
-
-//    // mark current page as active
-//    paginationLinks.forEach(link => {
-//        if (link.textContent === currentPage.toString()) {
-//            link.parentElement.classList.add('active');
-//        } else {
-//            link.parentElement.classList.remove('active');
-//        }
-//    });
-//}
-
-//// initialize pagination on page load
-//updatePaginationLinks();
