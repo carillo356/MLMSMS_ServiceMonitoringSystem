e7838d0a40cc89b70f08626599de348d8a293c66
Mintchi01 <127159876+Mintchi01@users.noreply.github.com>
2023-05-04 10:15:49 +0800

    Minor Changes

---
+++ MLMServiceMonitoringSystem/JS_Scripts/index/addRemove_services.js
@@ -0,0 +1,118 @@
+﻿/* Add and Remove Service*/
+function AddService(serviceName, resolve, reject) {
+    $.ajax({
+        url: "/Home/AddService",
+        type: "POST",
+        data: { serviceName: serviceName },
+        dataType: 'json',
+        success: function (response) {
+            if (response.success) {
+                $('#error-container-addservices').html('');
+                resolve();
+            }
+            else {
+                var errorContainer = document.getElementById("error-container");
+                errorContainer.innerHTML = `
+                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                          <strong><i class="fa fa-exclamation"></i> Error!</strong> <span>${response.errorMessage}</span>
+                          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>`;
+            }
+        },
+        error: function (xhr) {
+            var errorMessage = xhr.responseText;
+
+            var errorContainer = document.getElementById("error-container");
+            errorContainer.innerHTML = `
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        <strong><i class="fa fa-exclamation"></i> Error!</strong> <span>${errorMessage}</span>
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>`;
+            reject();
+        }
+    }).then(function () {
+        SynchServiceTB();
+    })
+}
+
+function RemoveAddedService(serviceName, command) {
+    // Show the confirmation box here
+    var modal = document.getElementById("alert-modal");
+    var modalTitle = document.getElementById("modal-title");
+    var modalMessage = document.getElementById("modal-message");
+    var btnYes = document.getElementById("btnYes");
+    var btnNo = document.getElementById("btnNo");
+
+    modal.style.display = "block";
+    modalTitle.innerHTML = "<i class='bi bi-exclamation-triangle-fill text-warning'></i> System Message";
+    modalMessage.innerHTML = "Are you sure you want to " + command.toUpperCase() + " '" + serviceName + "'?";
+    btnYes.onclick = function () {
+        $.ajax({
+            url: "/Home/DeleteAddedService",
+            type: "POST",
+            data: { serviceName: serviceName, command: command },
+            success: function (response) {
+                if (response.success) {
+                    ServicesInMonitor();
+
+                    var commandText = command.toUpperCase();
+                    if (command === "delete") {
+                        commandText = "DELETED";
+                    }
+
+                    var toastElement = document.createElement('div');
+                    toastElement.setAttribute('class', 'toast hide toast-stack');
+                    toastElement.setAttribute('role', 'alert');
+                    toastElement.setAttribute('aria-live', 'assertive');
+                    toastElement.setAttribute('aria-atomic', 'true');
+
+                    var toastBody = document.createElement('div');
+                    toastBody.setAttribute('class', 'toast-body');
+                    toastElement.appendChild(toastBody);
+
+                    var toastWrapper = document.getElementById('toast-wrapper');
+                    toastWrapper.appendChild(toastElement);
+
+                    var toast = new bootstrap.Toast(toastElement);
+                    var toastMessage = "You " + commandText + " the service " + "' " + serviceName + " '" + ".";
+                    toastBody.innerHTML = toastMessage;
+
+                    if (commandText === "DELETED") {
+                        // set background color to red if commandText is Deleted
+                        toast._element.classList.remove("bg-success");
+                        toast._element.classList.add("bg-danger");
+                    }
+
+                    toast.show();
+
+                    setTimeout(function () {
+                        toast.dispose();
+                    }, 4000);
+                } else {
+                    var errorContainer = document.getElementById("error-container");
+                    errorContainer.innerHTML = `
+                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                          <strong><i class="fa fa-exclamation"></i> Error!</strong> <span>${response.errorMessage}</span>
+                          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>`;
+                }
+            },
+            error: function (xhr) {
+                var errorMessage = xhr.responseText;
+
+                var errorContainer = document.getElementById("error-container");
+                errorContainer.innerHTML = `
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        <strong><i class="fa fa-exclamation"></i> Error!</strong> <span>${errorMessage}</span>
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>`;
+            }
+        }).then(function () {
+            SynchServiceTB();
+        });
+        modal.style.display = "none";
+    }
+    btnNo.onclick = function () {
+        modal.style.display = "none";
+    }
+}
---
+++ MLMServiceMonitoringSystem/JS_Scripts/index/submitForm_clearReset.js
@@ -0,0 +1,107 @@
+﻿$(document).ready(function () {
+
+    function clearAndReset() {
+        $("#error-container-addservices").html("");
+        $('input[type="checkbox"]:checked').each(function () {
+            $(this).prop("checked", false);
+        });
+        $('.search-box').val(''); // This line clears the input field with the search-box class
+        $('.search-box').trigger('input'); // This line triggers the input event
+    }
+
+    $("#btnCancelService").on("click", clearAndReset);
+    $("#btnCloseService").on("click", clearAndReset);
+});
+
+
+// Submit Form for Add Service
+function submitForm() {
+    var serviceNames = [];
+    var checkedCount = $('input[type="checkbox"]:checked').length;
+
+    $('input[type="checkbox"]:checked').each(function () {
+        serviceNames.push($(this).val());
+    });
+
+    if (serviceNames.length === 0) {
+        // Display error message if no services are selected
+        var errorContainer = document.getElementById("error-container-addservices");
+        errorContainer.innerHTML = '<span>Please select a service/s</span>'
+        return;
+    }
+
+    // Wrap each AddService call in a promise and store them in an array
+    var promises = serviceNames.map(function (serviceName) {
+        return new Promise(function (resolve, reject) {
+            AddService(serviceName, resolve, reject);
+        });
+    });
+    // Wait for all promises to complete
+    Promise.all(promises).then(function () {
+        SynchServiceTB();
+    });
+
+
+    if (serviceNames.length > 0) {
+        var toastElement = document.createElement('div');
+        toastElement.setAttribute('class', 'toast hide toast-stack');
+        toastElement.setAttribute('role', 'alert');
+        toastElement.setAttribute('aria-live', 'assertive');
+        toastElement.setAttribute('aria-atomic', 'true');
+
+        var toastBody = document.createElement('div');
+        toastBody.setAttribute('class', 'toast-body');
+        toastElement.appendChild(toastBody);
+
+        var toastWrapper = document.getElementById('toast-wrapper');
+        toastWrapper.appendChild(toastElement);
+
+        var toast = new bootstrap.Toast(toastElement);
+        var toastMessage = "You have added " + checkedCount + " service(s)";
+        toastBody.innerHTML = toastMessage;
+
+        // set background color to green if service/s has been added.
+        toast._element.classList.remove("text-bg-danger");
+        toast._element.classList.add("bg-success");
+
+        toast.show();
+
+        setTimeout(function () {
+            toast.dispose();
+        }, 4000);
+    }
+}
+
+// Selecting Rows Dropdown for Service Table
+const optionMenu2 = document.querySelector(".serviceRow"),
+    select_btn2 = optionMenu2.querySelector(".btnRow"),
+    options2 = optionMenu2.querySelectorAll(".optionRow"),
+    sBtn_text2 = optionMenu2.querySelector(".sBtnRow");
+
+select_btn2.addEventListener("click", () => optionMenu2.classList.toggle("active"));
+
+options2.forEach(option => {
+    option.addEventListener("click", () => {
+        let selectedOption = option.querySelector(".optionRow-text").innerText;
+        sBtn_text2.innerText = selectedOption;
+
+        optionMenu2.classList.remove("active");
+    });
+});
+
+// Selecting Rows Dropdown for Log History Table
+const optionMenuLogHistory = document.querySelector(".logHistoryRow"),
+    selectBtnLogHistory = optionMenuLogHistory.querySelector(".btnLogHistoryRow"),
+    optionsLogHistory = optionMenuLogHistory.querySelectorAll(".option_LogHistory"),
+    sBtnTextLogHistory = optionMenuLogHistory.querySelector(".sBtnLogHistoryRow");
+
+selectBtnLogHistory.addEventListener("click", () => optionMenuLogHistory.classList.toggle("active"));
+
+optionsLogHistory.forEach(option => {
+    option.addEventListener("click", () => {
+        let selectedOption = option.querySelector(".option_LogHistory-text").innerText;
+        sBtnTextLogHistory.innerText = selectedOption;
+
+        optionMenuLogHistory.classList.remove("active");
+    });
+});
