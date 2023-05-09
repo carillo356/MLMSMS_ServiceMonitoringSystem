f411d56bc8f0560b14b7cf95453d9c22b400600d
carillo356 <107926755+carillo356@users.noreply.github.com>
2023-05-09 14:04:32 +0800

    changes

--- OJTWindowsService/MultisoftServicesMonitor/MultisoftServicesMonitor.cs
+++ OJTWindowsService/MultisoftServicesMonitor/MultisoftServicesMonitor.cs
@@ -292,7 +292,7 @@ namespace MultisoftServicesMonitor
             if (_deleteLogsXDaysOld > 90)
                 _deleteLogsXDaysOld = 90;
 
-            checkCommandsIssuedTimer.Interval = /*5 * 60 * 1000*/18000; // every 12 seconds
+            checkCommandsIssuedTimer.Interval = 5 * 60 * 1000; //Every 5 mins
             checkCommandsIssuedTimer.Elapsed += new ElapsedEventHandler(OnCheckCommandsIssuedElapsedTime);
             checkCommandsIssuedTimer.Start();
 
